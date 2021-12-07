fdf10b037a0b71816e816df93c5752c45de08817
Atvars Karro <atvars.karro@nordigen.com>
2021-12-07 08:26:25 +0200

    Day 07, shorter

--- day07.py
+++ day07.py
@@ -1,22 +1,4 @@
-from math import inf
-
-data = list(map(int, open("data/day07.txt", "r").read().split(",")))
-
-
-m = inf
-for a in range(max(data)):
-    x = sum(abs(a-b) for b in data)
-    if x < m:
-        m = x
-print("Puzzle 7.1:", m)
-
-
-m = inf
-for a in range(max(data)):
-    x = 0
-    for b in data:
-        n = abs(a-b)
-        x += n * (n + 1) // 2
-    if m >= x:
-        m = x
-print("Puzzle 7.2:", m)
+d = list(map(int, open("data/day07.txt", "r").read().split(",")))
+r = range(min(d), max(d) + 1)
+print("Puzzle 7.1:", min(sum(abs(a - b) for b in d) for a in r))
+print("Puzzle 7.2:", min(sum(abs(a - b) * (abs(a - b) + 1) // 2 for b in d) for a in r))
